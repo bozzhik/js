b7e8e87a3f5a2f5414fb3b6f2a9068baade81184
max <bozzhik@ya.ru>
2022-10-31 02:01:29 +0300

    ById

--- js/eye.js
+++ js/eye.js
@@ -1,6 +1,2 @@
-$(document).mousemove(function(e) {
-    $('#cursor').css({
-      left:e.pageX - 1,
-      top:e.pageY - 1
-    });
-  });
+
+document.getElementById('text').innerHTML = 'Текст для отображения'
