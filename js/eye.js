e3cd335e354a3f8c47a84d96f1350be48ef771fe
max <bozzhik@ya.ru>
2022-10-31 02:38:59 +0300

    typeof

--- js/eye.js
+++ js/eye.js
@@ -1,2 +1,18 @@
+let max;
 
-document.getElementById('text').innerHTML = 'Текст для отображения'
+if (max === undefined) {
+    console.log('я типе нопишу ундифндед');
+}
+else {
+    console.log('я типе не напишу');
+}
+console.log(typeof max);
+max = 'мое имя';
+
+if (max === undefined) {
+    console.log('я типе нопишу ундифндед');
+}
+else {
+    console.log('я типе не напишу');
+}
+console.log(typeof max);
