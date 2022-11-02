9a76d6798fa5a9656d77390f26b1ae43e88757d9
max <bozzhik@ya.ru>
2022-11-02 22:38:17 +0300

    drag&drop

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -1,20 +1,41 @@
-function slidesPlugin(activeSlide) {
-    const  slides = document.querySelectorAll('.slide');
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
 
-    slides[1].classList.add('active');
-    
-    for (const slide of slides) {
-        slide.addEventListener('click', () => {
-            clearActiveClasses()
-            slide.classList.add('active')
-        })
-    }
-    
-    function clearActiveClasses () {
-        slides.forEach((slide) => {
-            slide.classList.remove('active')
-        })
-    }
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
 }
 
-slidesPlugin(1);
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add('hide'), 0)
+}
+
+function dragend(event) {
+    // event.target.className.remove('hold', 'hide');
+    event.target.className = 'item'
+}
+ 
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+}
+
+
