3b024cb21ed1a1b29c6981788b545abea12be95d
max <bozzhik@ya.ru>
2022-11-06 20:49:29 +0300

    ~functionExpression

--- js/test.js
+++ js/test.js
@@ -1,18 +1,4 @@
-// function calcSum(numOne, numTwo) {
-//    let result = 1
-
-//    for (let i = 0; i < numTwo; i++) {
-//       result *= numOne
-//    }
-//    return result
-// }
-
-// console.log(calcSum(2, 3))
-
-function calcSum(numOne, numTwo) {
-   if (numTwo === 1) return numOne
-   else {
-      return numOne * calcSum(numOne, numTwo - 1)
-   }
+let showMe = function () {
+   console.log(`крекер`)
 }
-console.log(calcSum(2, 3))
+showMe()
