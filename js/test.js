cd8608c299cad6e205b984fb1cdde0a814bcb9ff
max <bozzhik@ya.ru>
2022-11-06 17:39:12 +0300

    ~while~do$~for

--- js/test.js
+++ js/test.js
@@ -1,7 +1,17 @@
-let num = 0
-for (; num < 5; num++) {
-   if (num == 2) continue
+// let num = 0
+// while (num < 5) {
+//    console.log(num)
+//    num++
+// }
+
+// let num = 0
+// do {
+//    console.log(num)
+//    num++
+// } while (num < 5)
+
+for (var num = 0; num < 5; num++) {
    console.log(num)
 }
 
-console.log(`крекер под номером ${num}`)
+console.log(`я крекер номер ${num}`)
