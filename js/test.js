bd9e9dea4d6907bcb5980d23d262c56e7b87a79d
max <bozzhik@ya.ru>
2022-11-08 16:15:37 +0300

    @4~object

--- js/test.js
+++ js/test.js
@@ -1,7 +1,9 @@
 let max = {
    age: 13,
-   'bozzhik maxim': false
+   'bozzhik maxim': false,
+   cracker: true
 }
 
 console.log(max)
-console.log(!max['bozzhik maxim'])
+console.log(max['age'])
+console.log(max['cracker'])
