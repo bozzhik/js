3e8c2707cfcfb9f1419e670cb97e8491d5ac553f
max <bozzhik@ya.ru>
2022-11-08 18:22:26 +0300

    objectPropertiesCheck

--- js/test.js
+++ js/test.js
@@ -1,10 +1,8 @@
-// Object.assign(item(size),cracker(size),())
-
 let cracker = {
    taste: 'cheese',
    size: 13
 }
-console.log(cracker)
 
-Object.assign(cracker, { ['paper']: true, visibility: '0.25' })
-console.log(cracker)
+if (cracker.size) {
+   console.log(cracker.size)
+}
