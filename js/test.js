317b46e50b384687d7a1c7318d6de0d9005d087c
max <bozzhik@ya.ru>
2022-11-08 18:07:20 +0300

    objectPropertiesDelete

--- js/test.js
+++ js/test.js
@@ -2,19 +2,13 @@ let cracker = {
    taste: 'cheese'
 }
 
-console.log(cracker)
-
 cracker.size = 13
-
-console.log(cracker)
-
 cracker['visibility'] = true
-
-console.log(cracker)
-
 cracker.paper = {
    red: true,
    black: false
 }
+console.log(cracker)
 
-console.log(cracker.paper)
+delete cracker.taste
+console.log(cracker)
