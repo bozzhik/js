d497333e14ac31c9efbcbe26ca24265bb5cb75df
max <bozzhik@ya.ru>
2022-11-08 20:33:14 +0300

    functionObject~3
    add arrow function

--- js/test.js
+++ js/test.js
@@ -6,12 +6,10 @@ let cracker = {
       black: false
    },
 
-   // info: function () {
-   //    console.log(`${cracker.taste}, ${cracker.size} размер. С красным перцем - ${cracker.paper.red}`)
-   // },
-   info() {
-      console.log(`${this.taste}, ${this.size} размер. С красным перцем - ${this.paper.red}`)
+   showInfo() {
+      let info = () => console.log(`${this.taste}, ${this.size} размер. С красным перцем - ${this.paper.red}`)
+      info()
    }
 }
 
-cracker.info()
+cracker.showInfo()
