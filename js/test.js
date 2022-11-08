11f7e17a5f0cbd299de22f3b95ec89ecf7c06559
max <bozzhik@ya.ru>
2022-11-08 18:15:32 +0300

    objectDublicate

--- js/test.js
+++ js/test.js
@@ -1,12 +1,11 @@
+// Object.assign(item(size),cracker(size),())
+
 let cracker = {
    taste: 'cheese',
    size: 13
 }
-console.log(cracker)
 
-let item = cracker
-
-console.log(item)
+let item = Object.assign({}, cracker)
 
 item.size = 5
 
