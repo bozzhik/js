7b290ae102ad432a456d017cf7bb8ccb460967fc
max <bozzhik@ya.ru>
2022-11-10 21:03:24 +0300

    stringGetSymbol

--- js/test.js
+++ js/test.js
@@ -1,2 +1,8 @@
 let text = 'сырный крекер'
-console.log(text.length)
+let firstSymbol = text[0]
+let thirdSymbol = text[1]
+let lastSymbol = text[text.length - 1]
+
+console.log(firstSymbol)
+console.log(thirdSymbol)
+console.log(lastSymbol)
