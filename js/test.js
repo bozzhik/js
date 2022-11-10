90b2e2b2fb3241eb0a252c30a665821459d2c0ff
max <bozzhik@ya.ru>
2022-11-10 18:59:26 +0300

    parsInt & parsFloat

--- js/test.js
+++ js/test.js
@@ -1,3 +1,7 @@
-let valueOne = +'150'
+let valueOne = parseInt('150.59px')
 console.log(valueOne)
 console.log(typeof valueOne)
+
+let valueTwo = parseFloat('150.59px')
+console.log(valueTwo)
+console.log(typeof valueTwo)
