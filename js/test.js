fa4072a5ae95bb9286ce8f8e2710f2ec0e6c1597
max <bozzhik@ya.ru>
2022-11-10 19:01:03 +0300

    Math.random

--- js/test.js
+++ js/test.js
@@ -1,7 +1,2 @@
-let valueOne = parseInt('150.59px')
-console.log(valueOne)
-console.log(typeof valueOne)
-
-let valueTwo = parseFloat('150.59px')
-console.log(valueTwo)
-console.log(typeof valueTwo)
+console.log(Math.random())
+console.log(Math.random())
