fe5819dd6d26969b7204ad1dfef1ddf4a47cc262
max <bozzhik@ya.ru>
2022-11-10 14:33:46 +0300

    number~0

--- js/test.js
+++ js/test.js
@@ -1,10 +1,7 @@
-let userInfo = {
-   name: 'Вася',
-   аge: 30
-   showInfo() {
-      console.log(`$`)
-   }
-}
-// let user = userInfo
-// user.age = 45
-console.log(userInfo.age)
+// let num = 1000000
+let num = 1e6
+console.log(num)
+
+// let num2 = 0.00001
+let num2 = 1e-6
+console.log(num2)
