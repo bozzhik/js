350abf2690b63071253d540a9f62b57754347118
max <bozzhik@ya.ru>
2022-11-10 21:06:23 +0300

    stringGetAllSymbol

--- js/test.js
+++ js/test.js
@@ -1,8 +1,5 @@
 let text = 'сырный крекер'
-let firstSymbol = text[0]
-let thirdSymbol = text[1]
-let lastSymbol = text[text.length - 1]
 
-console.log(firstSymbol)
-console.log(thirdSymbol)
-console.log(lastSymbol)
+for (const char of text) {
+   console.log(char)
+}
