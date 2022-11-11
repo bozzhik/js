2190aac4b5f468ec42bd027efe090890c02c9ad3
max <bozzhik@ya.ru>
2022-11-11 23:28:00 +0300

    arrayElements

--- js/test.js
+++ js/test.js
@@ -1,4 +1,18 @@
-// let array = new Array()
-let array = ['сырный', 'крекер']
-
+let array = [
+   'крекер',
+   {
+      taste: 'cheese',
+      size: 13
+   },
+   true,
+   function () {
+      console.log('я крекер')
+   }
+]
 console.log(array)
+
+let matrix = [
+   [1, 2, 3],
+   [4, 5]
+]
+console.log(matrix)
