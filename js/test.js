ede34a38ea8cf11808276b85fdb64d7582044592
max <bozzhik@ya.ru>
2022-11-12 00:17:04 +0300

    arrayCopy&Add

--- js/test.js
+++ js/test.js
@@ -1,30 +1,5 @@
-// удалить элемент
+// скопировать часть массива + добавить элемент
 let array = ['сырный', 'соленый', 'острый']
 
-array.splice(1, 1)
-console.log(array)
-
-// удалить и вернуть в переменную
-let arrayTwo = ['сырный', 'соленый', 'острый']
-
-let removed = arrayTwo.splice(2, 1)
+let arrayTwo = array.concat('пикантный')
 console.log(arrayTwo)
-console.log(removed)
-
-// заменить элемент
-let arrayThree = ['сырный', 'соленый', 'острый']
-
-arrayThree.splice(1, 1, 'пикантный')
-console.log(arrayThree)
-
-// добаваить элемент
-let arrayFour = ['сырный', 'соленый', 'острый']
-
-arrayFour.splice(1, 0, 'пикантный')
-console.log(arrayFour)
-
-// еще удалить элемент
-let arrayFive = ['сырный', 'соленый', 'острый']
-
-arrayFive.splice(-1, 1)
-console.log(arrayFive)
