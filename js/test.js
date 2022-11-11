fc8f4003c8424fccab870eec4ea66247545758f0
max <bozzhik@ya.ru>
2022-11-12 00:07:51 +0300

    arrayManageElements

--- js/test.js
+++ js/test.js
@@ -1,7 +1,30 @@
+// удалить элемент
 let array = ['сырный', 'соленый', 'острый']
+
+array.splice(1, 1)
 console.log(array)
 
-delete array[1]
+// удалить и вернуть в переменную
+let arrayTwo = ['сырный', 'соленый', 'острый']
+
+let removed = arrayTwo.splice(1, 1)
 console.log(array)
-console.log(array[1])
-console.log(array.length)
+console.log(removed)
+
+// заменить элемент
+let arrayThree = ['сырный', 'соленый', 'острый']
+
+arrayThree.splice(1, 1, 'пикантный')
+console.log(arrayThree)
+
+// добаваить элемент
+let arrayFour = ['сырный', 'соленый', 'острый']
+
+arrayFour.splice(1, 0, 'пикантный')
+console.log(arrayFour)
+
+// еще удалить элемент
+let arrayFive = ['сырный', 'соленый', 'острый']
+
+arrayFive.splice(-1, 1)
+console.log(arrayFive)
