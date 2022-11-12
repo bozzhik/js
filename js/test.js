67e2a56c97674ffcc82de509e9501a92389e4f17
max <bozzhik@ya.ru>
2022-11-13 01:20:32 +0300

    arrayEnumerationFunction~2

--- js/test.js
+++ js/test.js
@@ -1,9 +1,7 @@
 let array = ['сырный', 'соленый', 'острый']
 
-// array.forEach(function (item, index, array) {
-//    console.log(`${item} размером ${index} находится в ${array}`)
-// })
+array.forEach(show)
 
-array.forEach((item, index, array) => {
-   console.log(`${item} размером ${index} находится в ${array}`)
-})
+function show(item) {
+   console.log(item)
+}
