322cb9b5c0cec54b8930421089c8a3d4b06a2cd1
max <bozzhik@ya.ru>
2022-11-13 01:16:25 +0300

    arrayEnumeration~2

--- js/test.js
+++ js/test.js
@@ -1,6 +1,5 @@
 let array = ['сырный', 'соленый', 'острый']
-console.log(array.length)
 
-for (let i = 0; i < array.length; i++) {
-   console.log(array[i])
+for (let arrayItem of array) {
+   console.log(arrayItem)
 }
