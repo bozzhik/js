e050f46237147c82cc50238e52f686e7560a4edf
max <bozzhik@ya.ru>
2022-11-17 00:53:27 +0300

    getElementsFunction *DOM

--- js/test.js
+++ js/test.js
@@ -1,35 +1,11 @@
-// класс
-const elemOne = document.querySelectorAll('.itemClass')
-console.log(elemOne)
+// конкретный элемент
+const elem = document.querySelectorAll('.itemClass h3')
+console.log(elem[2])
 
-// ид
-const elemOne2 = document.querySelectorAll('#itemId')
-console.log(elemOne2)
+// for (const item of elem) {
+//    console.log(item)
+// }
 
-// тэги
-const elemTwo = document.querySelectorAll('h3')
-console.log(elemTwo)
-
-// тэг с классом
-const elemThree = document.querySelectorAll('div.container')
-console.log(elemThree)
-
-// по первому уровню вложенности
-const elemFour = document.querySelectorAll('.itemClass > span')
-console.log(elemFour)
-
-// по нескольким селекторам
-const elemFive = document.querySelectorAll('.itemClass, #itemId')
-console.log(elemFive)
-
-// по вложенным селекторам
-const elemSix = document.querySelectorAll('.itemClass .text')
-console.log(elemSix)
-
-// по атрибуту
-const elemSeven = document.querySelectorAll('[data-item]')
-console.log(elemSeven)
-
-// по атрибуту со значением
-const elemEight = document.querySelectorAll('[data-item ="13"]')
-console.log(elemEight)
+elem.forEach((item) => {
+   console.log(item)
+})
