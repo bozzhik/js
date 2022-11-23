058e07010e525e85a194b3ed960016d6d8afae8d
max <bozzhik@ya.ru>
2022-11-23 13:32:00 +0300

    changeElementCode

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 const elem = document.querySelectorAll('.text')[0]
 
-const elemContent = elem.innerHTML
+const elemContent = elem.outerHTML
 console.log(elemContent)
 
-elem.innerHTML = `${elemContent}, я <b></b>бублик<b>`
-console.log(elem)
+elem.outerHTML = `я тоже <b>бублик<b>`
+console.log(elem) // код не обновляется, получаем старую запись
