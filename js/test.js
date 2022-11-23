00ce2608f42df830e54d9aa440a66d48a7aed61c
max <bozzhik@ya.ru>
2022-11-23 12:52:53 +0300

    live/static element's collection

--- js/test.js
+++ js/test.js
@@ -1,2 +1,8 @@
-const elem = document.getElementsByName('max')
-console.log(elem)
+const listStatic = document.querySelectorAll('.text')
+console.log(listStatic)
+
+const listLive = document.getElementsByClassName('text')
+console.log(listLive)
+
+const itemList = document.querySelector('.item-class')
+itemList.insertAdjacentHTML('beforeend', '<h4 class="text">я пряник</h4>')
