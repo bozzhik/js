6f3a5c370b62719d0f2f1f0ac23b98d0bdc4c39f
max <bozzhik@ya.ru>
2022-11-23 14:33:26 +0300

    removeElement *DOM

--- js/test.js
+++ js/test.js
@@ -1,5 +1,3 @@
 const elem = document.querySelector('.item-class')
-const cloneElem = elem.cloneNode(true) // если null то без содержимого
 
-const block = document.querySelector('.create')
-block.append(cloneElem)
+elem.remove()
