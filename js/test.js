27bb55de09e1b0678b6c246c09396231aee046e9
max <bozzhik@ya.ru>
2022-11-23 15:02:46 +0300

    changeTextStyle *DOM

--- js/test.js
+++ js/test.js
@@ -1,7 +1,5 @@
 const element = document.querySelector('span')
 
-element.style.color = 'yellow'
-element.style.textDecorationLine = 'underline'
-
-console.log(element.style.color) // получить стиль из ! HTML
-element.style.textDecorationLine = '' // обнулить стиль
+element.style.cssText = `
+color: yellow;
+text-decoration-line: underline;`
