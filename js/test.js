3a54e26d8c6b2c52d6d863b351da9205740efe19
max <bozzhik@ya.ru>
2022-11-23 14:19:47 +0300

    createCodeElement~ *DOM

--- js/test.js
+++ js/test.js
@@ -2,9 +2,15 @@ const elem = document.querySelector('.create')
 
 elem.insertAdjacentHTML('beforeend', ` <h1>я крекер</h1>`)
 
-/*
- *! beforebegin - перед элементом
- *! afterbegin - в начало элемента
- *! beforeend - в конец элемента
- *! afterend - после элемента
+/**
+ * * beforebegin - перед элементом
+ * * afterbegin - в начало элемента
+ * ! элемент
+ * * beforeend - в конец элемента
+ * * afterend - после элемента
+ */
+
+/**
+ * ! используется чаще inserAdjacentHTML
+ * * остальное через: apppent prepend before after
  */
