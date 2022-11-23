36ed007c56d5d487767f1c8c77c049b7e6df9502
max <bozzhik@ya.ru>
2022-11-23 16:04:01 +0300

    checkHasAttribute *DOM

--- js/test.js
+++ js/test.js
@@ -1,6 +1,9 @@
 const link = document.querySelector('#me')
 
-link.hasAttribute('name') // проверить наличие атрибута
-link.getAttribute('name') // получить артибут
-link.setAttribute('name', 'telegram') // установить атрибут
-link.removeAttribute('name') // удалить атрибут
+link.setAttribute('data-link', 'telegram')
+
+if (link.hasAttribute('data-link')) {
+   console.log('data-link имеется')
+} else {
+   console.log(false)
+}
