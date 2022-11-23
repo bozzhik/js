1a6d80b2d54279ebfb28cd65b3dd71973e3af1e7
max <bozzhik@ya.ru>
2022-11-23 15:14:57 +0300

    getCssStyle~2 *DOM

--- js/test.js
+++ js/test.js
@@ -1,6 +1,9 @@
-const element = document.querySelector('span')
+const element = document.querySelector('.cracker')
 
 console.log(element.style.color) // в html style его нет
 
 const elementStyle = getComputedStyle(element) // берем из css
 console.log(elementStyle.color)
+
+const elementBeforeStyle = getComputedStyle(element, '::before')
+console.log(elementBeforeStyle.color)
