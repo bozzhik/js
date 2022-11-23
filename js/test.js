0325a72482fcf483c51e84fb1b6ca86022bdfb06
max <bozzhik@ya.ru>
2022-11-23 14:06:03 +0300

    pasteSomeElements *DOM

--- js/test.js
+++ js/test.js
@@ -4,10 +4,4 @@ const newElement = document.createElement('div')
 newElement.innerHTML = `я коклетка`
 console.log(newElement)
 
-// elem.before(newElement) // перед элементом
-
-// elem.after(newElement) // после элемента
-
-// elem.prepend(newElement) // внутрь в начало элемента
-
-elem.append(newElement) // внутрь в конец элемента
+elem.append(newElement, 'куриная')
