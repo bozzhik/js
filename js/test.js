75b99348e6a4f2f0bbc80306a2b9fe5e42d9ed3c
max <bozzhik@ya.ru>
2022-11-23 15:00:45 +0300

    get/removeStyle *DOM

--- js/test.js
+++ js/test.js
@@ -2,3 +2,6 @@ const element = document.querySelector('span')
 
 element.style.color = 'yellow'
 element.style.textDecorationLine = 'underline'
+
+console.log(element.style.color) // получить стиль из ! HTML
+element.style.textDecorationLine = '' // обнулить стиль
