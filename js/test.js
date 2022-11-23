145e7e566ab9f97e8f6f69c0b929f6eb138c7402
max <bozzhik@ya.ru>
2022-11-23 14:03:14 +0300

    pasteElement *DOM

--- js/test.js
+++ js/test.js
@@ -1,2 +1,13 @@
-const newText = document.createTextNode('вафелька')
-console.log(newText)
+const elem = document.querySelector('.create')
+const newElement = document.createElement('div')
+
+newElement.innerHTML = `я коклетка`
+console.log(newElement)
+
+// elem.before(newElement) // перед элементом
+
+// elem.after(newElement) // после элемента
+
+// elem.prepend(newElement) // внутрь в начало элемента
+
+elem.append(newElement) // внутрь в конец элемента
