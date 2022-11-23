3a177944f7125d6996e24ee59a92efa8890445a6
max <bozzhik@ya.ru>
2022-11-23 13:12:27 +0300

    getElementNext *DOM

--- js/test.js
+++ js/test.js
@@ -1,8 +1,4 @@
-const elems = document.querySelectorAll('.atribute')
-for (let elem of elems) {
-   if (elem.matches('[data-item$="5"]')) {
-      console.log('есть 5')
-   } else if (elem.matches('[data-item$="13"]')) {
-      console.log('есть и 13')
-   }
-}
+const elem = document.querySelector('.item-class')
+const elem2 = elem.nextElementSibling
+
+console.log(elem2)
